c9fa361dc78294f9d9254a633a57e78354c019ce
Jimena <j.miguez@airlst.com>
2023-12-13 11:57:11 +0100

    Add video hero to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,22 @@
-<template><p class="text-pink-500">Hello, World!</p></template>
+<template>
+  <section class="relative w-full h-screen overflow-hidden">
+    <video
+      autoplay
+      loop
+      muted
+      playsinline
+      class="absolute top-0 left-0 w-full h-full object-cover"
+    >
+      <source src="~/assets/videos/hero.mp4" type="video/mp4" />
+      <source src="~/assets/videos/hero.webm" type="video/webm" />
+      Your browser does not support the video tag.
+    </video>
+    <section
+      class="absolute top-0 left-0 w-full h-full flex justify-center items-center"
+    >
+      <hgroup class="text-white text-center">
+        <h1 class="text-4xl font-bold">Mastering Grunge</h1>
+      </hgroup>
+    </section>
+  </section>
+</template>
