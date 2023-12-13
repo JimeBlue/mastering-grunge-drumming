725689af3de7605c5df7d619ba1af5fa09ae5c19
Jimena <j.miguez@airlst.com>
2023-12-13 13:47:51 +0100

    Update hero video + add content and style to index page

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,18 @@
       <source src="~/assets/videos/hero.webm" type="video/webm" />
       Your browser does not support the video tag.
     </video>
-    <section
-      class="absolute top-0 left-0 w-full h-full flex justify-center items-center"
-    >
-      <hgroup class="text-white text-center">
-        <h1 class="text-4xl font-bold">Mastering Grunge</h1>
+    <section class="absolute top-0 left-0 w-full h-full">
+      <hgroup
+        class="w-full h-full container flex flex-col justify-around items-start text-white"
+      >
+        <h1 class="text-6xl lg:text-8xl font-normal max-w-md">
+          Mastering Grunge Drumming
+        </h1>
+        <button class="hidden md:block">Our Covers</button>
+        <p class="text-2xl font-normal max-w-md">
+          Unlock The Power of Grunge Drums by Covering the Iconic Sounds of the
+          90s.
+        </p>
       </hgroup>
     </section>
   </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,19 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    ({ addComponents, theme }) => {
+      addComponents({
+        '.container': {
+          width: '100%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.5'),
+          paddingRight: theme('spacing.5'),
+          maxWidth: '1240px',
+        },
+      });
+    },
+  ],
 };
