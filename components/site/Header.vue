9e9c99dba3b7aa34062df2eec7570329f8c1d044
Jimena <j.miguez@airlst.com>
2023-12-15 17:33:05 +0100

    Add new header with collapsibles

--- components/site/Header.vue
+++ components/site/Header.vue
@@ -1,92 +1,62 @@
 <template>
-  <header class="bg-indigo-600">
-    <nav class="container px-6 py-8 mx-auto">
-      <div class="flex items-center justify-between">
-        <NuxtLink
-          to="/"
-          class="text-xl font-bold text-gray-100 md:text-2xl hover:text-indigo-400"
-          >Logo
-        </NuxtLink>
-        <div @click="toggleNav" class="flex">
-          <button
-            type="button"
-            class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400"
-          >
-            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-              <path
-                fill-rule="evenodd"
-                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-              ></path>
-            </svg>
-          </button>
-        </div>
+  <header class="bg-white p-4">
+    <section class="flex justify-between items-center">
+      <p>Logo</p>
+      <button class="menu-button" @click="showOverlay = true">Menu</button>
+    </section>
+
+    <section
+      v-if="showOverlay"
+      class="fixed inset-0 bg-black p-4 md:h-fit md:inset-x-0 md:top-0"
+      :class="{ 'z-50': showOverlay }"
+    >
+      <!-- Overlay's header-->
+      <div class="flex justify-between items-center">
+        <p class="text-white">Logo</p>
+        <button class="text-white" @click="showOverlay = false">Close</button>
       </div>
 
-      <ul
-        v-if="showMenu"
-        v-for="band in bands"
-        :key="band.slug"
-        class="flex-col mt-8 space-y-4"
-      >
-        <li class="text-gray-100 hover:text-indigo-400">
-          <div class="relative" @click="toggleMenu(band.slug)">
-            <button class="py-5 px-3 inline-flex items-center">
-              <span>{{ band.title }}</span>
-              <svg
-                class="ml-2 w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
+      <!-- Bands with Collapsibles in songs titles -->
+      <div>
+        <p class="text-white">Our Bands</p>
+        <ul v-for="band in bands" :key="band.slug" class="text-white">
+          <div
+            class="flex justify-between items-center cursor-pointer p-4 text-white"
+            @click="toggle(band.slug)"
+          >
+            <span>{{ band.title }}</span>
+            <span>{{ collapsedSlugs.includes(band.slug) ? '+' : '-' }}</span>
+          </div>
+          <div v-if="!collapsedSlugs.includes(band.slug)" class="flex flex-col">
             <NuxtLink
               v-for="(song, index) in band.songs"
               :key="song.slug"
-              v-if="menusOpenRefs[band.slug].value"
               :to="`/cover/band/${band.slug}/song/${song.slug}`"
-              class="overflow-hidden flex flex-col"
+              @click="showOverlay = false"
             >
-              <a href="#" class="block px-4 py-2 text-black">{{
-                song.title
-              }}</a>
+              {{ song.title }}
             </NuxtLink>
           </div>
-        </li>
-      </ul>
-    </nav>
+        </ul>
+      </div>
+    </section>
   </header>
 </template>
+
 <script setup>
 const { bands } = useCover();
 
-// functionality to open/close navbar
-const showMenu = ref(false);
-const toggleNav = () => (showMenu.value = !showMenu.value);
+const showOverlay = ref(false);
 
-// functionality for collapsibles
-const menusOpenRefs = {};
-bands.forEach((band) => {
-  menusOpenRefs[band.slug] = ref(false);
-});
-const toggleMenu = (slug) => {
-  menusOpenRefs[slug].value = !menusOpenRefs[slug].value;
+// Functionality for collapsibles
+const collapsedSlugs = ref([]);
+const toggle = (slug) => {
+  const index = collapsedSlugs.value.indexOf(slug);
+  if (index > -1) {
+    collapsedSlugs.value.splice(index, 1);
+  } else {
+    collapsedSlugs.value.push(slug);
+  }
 };
 </script>
-<style scope>
-/* we will explain what these classes do next! */
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-</style>
+<style scope></style>
