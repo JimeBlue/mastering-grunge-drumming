36c61ded5909a4722454ba0949cc5d512342881a
Jimena <j.miguez@airlst.com>
2023-12-26 11:45:38 +0100

    Add btn and styles to index page

--- pages/index.vue
+++ pages/index.vue
@@ -15,20 +15,25 @@
     <section class="absolute top-0 left-0 w-full h-full">
       <SiteHeader />
       <hgroup
-        class="w-full h-full container flex flex-col justify-around items-start"
+        class="w-full h-full container py-6 md:py-0 md:flex md:flex-col md:justify-around"
       >
-        <h1 class="text-6xl lg:text-8xl font-normal max-w-md text-primary-400">
+        <h1 class="text-6xl lg:text-8xl font-normal max-w-md text-primary-400 mb-36 md:mb-0">
           Mastering Grunge Drumming
         </h1>
-        <NuxtLink to="/cover"
-          ><button class="btn btn-primary-animated px-6 py-4">
+        <NuxtLink to="/cover" class="hidden md:block"
+          ><button class="btn btn-primary-animated">
             Our Covers
           </button></NuxtLink
         >
-        <p class="text-2xl font-normal max-w-md text-white">
+        <p class="text-2xl font-normal max-w-md text-white mb-10 md:pb-6">
           Unlock The Power of Grunge Drums by Covering the Iconic Sounds of the
           90s.
         </p>
+        <NuxtLink to="/cover" class="md:hidden"
+          ><button class="text-white">
+            Our Covers
+          </button></NuxtLink
+        >
       </hgroup>
     </section>
   </section>
